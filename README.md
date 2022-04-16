9dd503ac51867da0f8593d255612fe584a4b3211
xcesco <xcesco@gmail.com>
2022-04-17 00:22:04 +0200

    @wip

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # connect4
 Implementation of connect4 game (Forza 4).
+
+```mermaid
+sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->>John: Hello John, how are you?
+    loop Healthcheck
+        John->>John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts <br/>prevail!
+    John-->>Alice: Great!
+    Arturo-->>Benny: Davvero??
+    John->>Bob: How about you?
+    Bob-->>John: Jolly good!
+```
--- connect4-core/src/main/java/it/fmt/games/connect4/model/operators/AvailableMovesFinder.java
+++ connect4-core/src/main/java/it/fmt/games/connect4/model/operators/AvailableMovesFinder.java
@@ -1,33 +1,38 @@
 package it.fmt.games.connect4.model.operators;
 
-import it.fmt.games.connect4.model.*;
+import it.fmt.games.connect4.model.Board;
+import it.fmt.games.connect4.model.Cell;
+import it.fmt.games.connect4.model.Coordinates;
+import it.fmt.games.connect4.model.Piece;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class AvailableMovesFinder extends AbstractBoardOperator {
 
-    private AvailableMovesFinder(Board board, Piece piece) {
-        super(board, piece);
-    }
+  private AvailableMovesFinder(Board board, Piece piece) {
+    super(board, piece);
+  }
 
-    public static List<Coordinates> findMoves(Board board, Piece piece) {
-        AvailableMovesFinder finder = new AvailableMovesFinder(board, piece);
-        return finder.findMoves();
-    }
+  public static List<Coordinates> findMoves(Board board, Piece piece) {
+    AvailableMovesFinder finder = new AvailableMovesFinder(board, piece);
+    return finder.findMoves();
+  }
 
-    private List<Coordinates> findMoves() {
-        return board.getCellStream()
-                .parallel()
-                .filter(this::isValidCellForInsert)
-                .map(Cell::getCoordinates)
-                .collect(Collectors.toList());
-    }
+  private List<Coordinates> findMoves() {
+    return board.getCellStream()
+            .parallel()
+            .filter(this::isValidCellForInsert)
+            .map(Cell::getCoordinates)
+            .sorted(Comparator.comparing(Coordinates::toString))
+            .collect(Collectors.toList());
+  }
 
-    private boolean isValidCellForInsert(Cell cell) {
-        return board.isCellContentEqualsTo(cell.getCoordinates(), Piece.EMPTY) &&
-                isLowerCellFilled(cell.getCoordinates());
-    }
+  private boolean isValidCellForInsert(Cell cell) {
+    return board.isCellContentEqualsTo(cell.getCoordinates(), Piece.EMPTY) &&
+            isLowerCellFilled(cell.getCoordinates());
+  }
 
 
 }
